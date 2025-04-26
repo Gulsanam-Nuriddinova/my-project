ba32c66cd112b3b0f24b8cc1290a34893320e5a5
Gulsanam-Nuriddinova <gulsanamnuriddinova56@gmail.com>
2025-04-26 23:19:44 +0500

    init

--- src/ftt-js/main.js
+++ src/ftt-js/main.js
@@ -1,23 +1,3 @@
-// function getRandomItem(arr) {
-//     return arr[Math.floor(Math.random() * arr.length)];
-// }
-
-// function getUniqueColors(count) {
-//     const shuffled = [...colors].sort(() => Math.random() - 0.5);
-//     return shuffled.slice(0, count);
-// }
-
-// function showGameOver() {
-//     gameOverScreen.classList.remove('hidden');
-// }
-
-// function hideGameOver() {
-//     gameOverScreen.classList.add('hidden');
-//     generateNewRound();
-// }
-
-// generateNewRound();
-
 const correctText = document.getElementById('correct-text');
 const optionsContainer = document.getElementById('options-container');
 const gameOverScreen = document.getElementById('game-over');
@@ -73,5 +53,4 @@ function hideGameOver() {
     generateNewRound();
 }
 
-// O'yinni boshlash
 generateNewRound();
